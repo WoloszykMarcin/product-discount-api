bb9eae21fd8da5e2f6856b17f286a86ca42f92e8
Marcin Woloszyk <marcinwoloszyk412@gmail.com>
2024-07-16 10:52:05 +0200

    initial commit

---
+++ src/main/java/com/apis/productdiscountapi/ProductDiscountApiApplication.java
@@ -0,0 +1,13 @@
+package com.apis.productdiscountapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductDiscountApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductDiscountApiApplication.class, args);
+	}
+
+}
