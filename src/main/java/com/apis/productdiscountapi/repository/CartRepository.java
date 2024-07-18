ef4c45f25f497a85cc2c1ae1def394e737c525c0
Marcin Woloszyk <marcinwoloszyk412@gmail.com>
2024-07-18 13:11:38 +0200

    optimistic locking

--- src/main/java/com/apis/productdiscountapi/repository/CartRepository.java
+++ src/main/java/com/apis/productdiscountapi/repository/CartRepository.java
@@ -2,13 +2,16 @@ package com.apis.productdiscountapi.repository;
 
 import com.apis.productdiscountapi.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 
+import javax.persistence.LockModeType;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface CartRepository extends JpaRepository<Cart, UUID> {
 
+    @Lock(LockModeType.OPTIMISTIC)
     @Query("SELECT c FROM Cart c LEFT JOIN FETCH c.items WHERE c.id = :id")
     Optional<Cart> findByIdWithItems(UUID id);
 
